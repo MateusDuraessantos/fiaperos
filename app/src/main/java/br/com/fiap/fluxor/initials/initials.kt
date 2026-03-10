6b2854b056824a9b96ae204cfa130c7446d83bff
raelzinsk <raelcapelati@gmail.com>
2026-03-09 22:51:05 -0300

    pagina inicial

---
+++ app/src/main/java/br/com/fiap/fluxor/initials/initials.kt
@@ -0,0 +1,195 @@
+package br.com.fiap.fluxor.initials
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.FloatingActionButtonDefaults.elevation
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.modifier.modifierLocalOf
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import br.com.fiap.fluxor.Codec
+import br.com.fiap.fluxor.LoginScreen
+import br.com.fiap.fluxor.R
+import br.com.fiap.fluxor.ui.theme.FluxORTheme
+import org.w3c.dom.Text
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            FluxORTheme {
+                InitialsScreen()
+            }
+        }
+    }
+}
+
+@Composable
+fun InitialsScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFFFFFFFF))
+    ){
+        Card(
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(30.dp),
+            shape = RoundedCornerShape(0.dp),
+            colors = CardDefaults.cardColors(containerColor = Color(0xFF001D4D)),
+        ) { }
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(top = 50.dp, start = 20.dp, end = 20.dp)
+        ) {
+            Text(
+                text = "Desempenho Operacional",
+                modifier = Modifier,
+                fontSize = 13.sp,
+                fontWeight = FontWeight.Bold,
+                color = Color.Black,
+                fontFamily = Codec
+
+            )
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Card(
+                modifier = Modifier
+                    .weight(1f)
+                    .height(200.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                shape = RoundedCornerShape(16.dp),
+                colors = CardDefaults.cardColors(containerColor = Color(0xFFFCFAFD)),
+            ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.score),
+                            contentDescription = "Logo escrito FluxOR",
+                            modifier = Modifier.size(140.dp)
+                        )
+                    }
+
+                }
+                Card(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(200.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFFCFAFD)),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.eficiencia),
+                            contentDescription = "Logo escrito FluxOR",
+                            modifier = Modifier.size(140.dp)
+                        )
+                    }
+
+                }
+            }
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 10.dp),
+                horizontalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Card(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(200.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFFCFAFD)),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.qualidade),
+                            contentDescription = "Logo escrito FluxOR",
+                            modifier = Modifier.size(140.dp)
+                        )
+                    }
+
+                }
+                Card(
+                    modifier = Modifier
+                        .weight(1f)
+                        .height(200.dp),
+                    elevation = CardDefaults.cardElevation(defaultElevation = 8.dp),
+                    shape = RoundedCornerShape(16.dp),
+                    colors = CardDefaults.cardColors(containerColor = Color(0xFFFCFAFD)),
+                ) {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        verticalArrangement = Arrangement.Center
+                    ) {
+                        Image(
+                            painter = painterResource(id = R.drawable.tempo),
+                            contentDescription = "Logo escrito FluxOR",
+                            modifier = Modifier.size(140.dp)
+                        )
+                    }
+
+                }
+            }
+
+        }
+
+
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun InitialsScreenPreview() {
+    FluxORTheme {
+        InitialsScreen()
+    }
+}
